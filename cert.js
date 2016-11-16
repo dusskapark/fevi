2cf136d6bc324a9397296e45c6a1f7e8df043e66
JooHyungPark <dusskapark@gmail.com>
2016-11-16 14:22:37 +0900

    bugfix

--- cert.js
+++ cert.js
@@ -1,11 +1,21 @@
-var letsencrypt = require('letsencrypt-express');
+'use strict';
 
-letsencrypt.create({
-    server: 'https://acme-staging.api.letsencrypt.org/directory',
-    email: 'dusskapark@gmail.com',
-    agreeTos: true,
-    approveDomains: 'ec2-52-78-228-8.ap-northeast-2.compute.amazonaws.com',
+require('letsencrypt-express').create({
+
+    server: 'staging'
+
+    ,
+    email: 'dusskapark@gmail.com'
+
+    ,
+    agreeTos: true
+
+    ,
+    approveDomains: ['ec2-52-78-228-8.ap-northeast-2.compute.amazonaws.com']
+
+    ,
     app: require('express')().use('/', function(req, res) {
         res.end('Hello, World!');
     })
+
 }).listen(80, 443);
